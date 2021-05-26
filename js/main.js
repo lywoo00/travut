13ccb44b07d30c8dca85e023d530245f1a3afb3d
lywoo00 <lywoo00@naver.com>
2021-05-26 15:25:49 +0900

    subpage 작업

--- js/main.js
+++ js/main.js
@@ -8,14 +8,16 @@ $(function(){
             $('.content-pop.left').stop().fadeIn(300);
             $('.etc-img').stop().fadeIn(300);
             $('.click-me').stop().fadeOut(100);
-            $('.arrow-box').stop().animate({opacity: '0'},100);
+            $('.download').stop().fadeOut(100);
+            $('.content_left .arrow-box').stop().animate({opacity: '0'},100);
             $('.content_right').stop().animate({opacity: '0'},100);
         } else {
             // $(this).stop().animate({scale:'1'},300);
             $('.content-pop.left').stop().fadeOut(300);
             $('.etc-img').stop().fadeOut(300);
             $('.click-me').stop().fadeIn(100);
-            $('.arrow-box').stop().animate({opacity: '1'},100);
+            $('.download').stop().fadeIn(100);
+            $('.content_left .arrow-box').stop().animate({opacity: '1'},100);
             $('.content_right').stop().fadeIn(100);
             $('.content_right').stop().animate({opacity: '1'},100);
         }
@@ -27,6 +29,7 @@ $(function(){
             $('.content-pop.right').stop().fadeIn(300);
             $('.etc-img').stop().fadeIn(300);
             $('.click-me').stop().fadeOut(100);
+            $('.download').stop().fadeOut(100);
             $('.arrow-box').stop().animate({opacity: '0'},100);
             $('.content_left').stop().animate({opacity: '0'},100);
         } else {
@@ -34,6 +37,7 @@ $(function(){
             $('.content-pop.right').stop().fadeOut(300);
             $('.etc-img').stop().fadeOut(300);
             $('.click-me').stop().fadeIn(100);
+            $('.download').stop().fadeIn(100);
             $('.arrow-box').stop().animate({opacity: '1'},100);
             $('.content_left').stop().animate({opacity: '1'},100);
         }
